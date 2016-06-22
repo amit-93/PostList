8c1987875e4fccfc0e10a131c1750c5d98fe8468
amit-93 <amit.ishu@gmail.com>
2016-06-22 08:53:17 +0530

    Update Formatter.cs

--- PostList/PostList/Formatter/Formatter.cs
+++ PostList/PostList/Formatter/Formatter.cs
@@ -7,10 +7,6 @@ namespace PostList
     public class Formatter
     {
 
-        static List<string> ConfigKey;
-        static Dictionary<string, string> ConfigValue;
-
-
         public static IFormatter GetFormatter(string Format)
         {
             string ClassName = "";
@@ -19,20 +15,15 @@ namespace PostList
 
             try
             {
-                ConfigKey = new List<string>();
-                ConfigKey.Add(Constant.Plain);
-                ConfigKey.Add(Constant.Json);
-                ConfigKey.Add(Constant.Html);
-                ConfigValue = AppConfig.ReadConfig(ConfigKey);
                 AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
 
-                if (ConfigValue == null || ConfigValue.Count == 0 || string.IsNullOrWhiteSpace(AssemblyName))
+                if (AppConfig.ConfigValue == null || AppConfig.ConfigValue.Count == 0 || string.IsNullOrWhiteSpace(AssemblyName))
                 {
                     ExceptionHandling.ShowException(Constant.Error);
                     return null;
                 }
 
-                ClassName = ConfigValue[Format];
+                ClassName = AppConfig.ConfigValue[Format];
                 Assembly asm = Assembly.Load(AssemblyName); 
                 Type type = asm.GetType(AssemblyName+"."+ ClassName);
                 var obj = Activator.CreateInstance(type);
